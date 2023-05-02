a1c682e7d8f5da6f5300fa99f8f8adb60ee0b24e
Arvid Pettersson <Arvid.P@bahnhof.se>
2023-05-02 14:12:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+const express = require("express")
+const fu = require("express-fileupload");
+var cors = require('cors')
+var bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static("public"));
+app.use(bodyParser.json())
+app.use(express.json());
+app.use(fu());
+
+
+
+function initRoutes(){
+
+ app.get("/",showAll);
+
+ app.post("/create", createArticle)
+
+ app.post("/addImage", addImage)
+
+
+
+
+}
+
+app.listen(5000,()=>{
+
+ console.log("connected to port 5000")
+
+})
+
+const uri = process.env.mongo
+
+
+const client = new MongoClient(uri, {
+
+ serverApi: {
+
+ version: ServerApiVersion.v1,
+
+ strict: true,
+
+ deprecationErrors: true,
+
+ }
+
+});
+
+const db = client.db("HögskolanDB");
+
+const col = db.collection("articles");
+
+//console.log("collection", col);
+
+run();
+
+async function run() {
+
+try {
+
+ // Connect the client to the server (optional starting in v4.7)
+
+ console.log("before connect");
+
+ await client.connect();
+ /* client.db("HögskolanDB").dropCollection("articles") */
+ console.log("after connect");
+initRoutes();
+
+ }
+
+ catch(err){
+
+  console.log("connection", err.message);
+
+ }
+
+}
+
+async function createArticle(req,res){
+
+let body = req.body;
+
+}
+
+async function addImage(req, res){
+
+    let {img} = req.files;
+     
+
+    let {title,category,description,deepDescription, contact, favorite, orderForm,videos} = req.body;
+
+    let imageName = img.name.split(".");
+    let length = imageName.length;
+    let url = "pictures/"+title+"."+imageName[(length-1)];
+    req.files.img.mv("public/"+url);
+
+    let article = {title:title,category:category,description:description,deepDescription:deepDescription, contact:JSON.parse(contact), favorite:JSON.parse(favorite), orderForm:JSON.parse(orderForm),videos:JSON.parse(videos), url:url}
+
+    console.log(article)
+
+    const p = await col.insertOne(article)
+
+    let result = await col.findOne({_id:p.insertedId})
+
+    res.send(result);
+
+
+}
+
+
+async function showAll(req,res){
+
+    console.log(col);
+
+    try{
+
+        let result = await col.find().toArray();
+
+            res.send(result);
+
+        }
+
+    catch(err){
+
+        res.send(err);
+
+    }
+}
